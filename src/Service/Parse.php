fe36c582da87c33053e3c2ef0221cc5091691096
Équipe Pyréweb <support@pyreweb.com>
2026-03-30 11:05:40 +0200

    feat: Ajouter la classe Parse pour la conversion de valeurs hexadécimales en tableau RGB

---
+++ src/Service/Parse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Pyreweb\Chroma\Service;
+
+/**
+ * @author Hugo Doueil <hugo@pyreweb.com>
+ * @author Pyréweb <contact@pyreweb.com>
+ */
+class Parse
+{
+	public const HEX_SHORT_LENGTH = 3;
+
+	public static function hex(string $hex): array
+	{
+		$hex = ltrim($hex, '#');
+
+		if (strlen($hex) === self::HEX_SHORT_LENGTH) {
+			$hex = $hex[0] . $hex[0] . $hex[1] . $hex[1] . $hex[2] . $hex[2];
+		}
+
+		return [
+			hexdec(substr($hex, 0, 2)),
+			hexdec(substr($hex, 2, 2)),
+			hexdec(substr($hex, 4, 2)),
+		];
+	}
+}
