ee70577979c117293fccd8aaa5389a9e68794301
Équipe Pyréweb <support@pyreweb.com>
2026-03-30 14:00:55 +0200

    feat: Supprimer l'importation de la classe Validate dans Color.php

--- src/Enum/Color.php
+++ src/Enum/Color.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Pyreweb\Chroma\Enum;
 
 use Pyreweb\Chroma\Service\Convert;
-use Pyreweb\Chroma\Service\Validate;
 
 /**
  * @author Hugo Doueil <hugo@pyreweb.com>
